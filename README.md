39e1fbc029cfb7a56f0a23b2d1ae38aff5db2ba3
Joseph Aje <ajejoseph22@gmail.com>
2023-08-13 17:56:18 +0100

    chore: prettified

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # foursquare-finder
+
+## Description
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
 import "dotenv/config";
 import yargs from "yargs";
-
 (async () => {
-  yargs
-      .commandDir("./lib/commands")
-      .demandCommand(1, "Did you forget to specify a command?")
-      .recommendCommands()
-      .showHelpOnFail(true, "Specify --help for available options")
-      .strict(true)
-      .help()
-      .wrap(yargs.terminalWidth()).argv;
+    yargs
+        .commandDir("./lib/commands")
+        .demandCommand(1, "Did you forget to specify a command?")
+        .recommendCommands()
+        .showHelpOnFail(true, "Specify --help for available options")
+        .strict(true)
+        .help()
+        .wrap(yargs.terminalWidth()).argv;
 })();
